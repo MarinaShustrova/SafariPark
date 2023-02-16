3cb6c84c0ed414b81baec83bed061c86d20bc2e0
MarinaShustrova <averc.marina@yandex.ru>
2023-02-16 12:41:16 +0300

    add logic routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-require('@babel/register');
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+// const hbs = require('hbs');
 
-const session  = require('express-session');
+const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
 const sessionConfig = {
@@ -18,18 +18,41 @@ const sessionConfig = {
   },
 };
 
-//*  далее будут подключены роутеры
+const getAllCategories = require('./middlewares/getAllCategories.middleware');
+
+const indexRouter = require('./routes/index.router');
+const adminsRouter = require('./routes/admins.router');
+const categoriesRouter = require('./routes/categories.router');
+const tariffsRouter = require('./routes/tariffs.router');
+const newsRouter = require('./routes/news.router');
+const scheduleRouter = require('./routes/schedule.router');
+
+const chatRouter = require('./routes/chats.router');
+const staffRouter = require('./routes/staff.router')
 
 const app = express();
 app.use(session(sessionConfig));
 
+// app.set('view engine', 'hbs');
 app.set('views', path.join(process.env.PWD, 'src', 'views'));
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+hbs.registerPartials(path.join(process.env.PWD, 'src/views/partials'));
 
-//* app.use.....
+app.use(getAllCategories);
+
+app.use('/', indexRouter);
+app.use('/admins', adminsRouter);
+app.use('/categories', categoriesRouter);
+app.use('/tariffs', tariffsRouter);
+app.use('/news', newsRouter);
+app.use('/schedule', scheduleRouter);
+
+app.use('/chat', chatRouter);
+app.use('/staff', staffRouter)
+
 
 module.exports = app;
